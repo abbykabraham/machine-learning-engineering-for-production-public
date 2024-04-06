ae2b2e4e0be6b9222c514db57944c86b208f17df
abbykabraham <57812374+abbykabraham@users.noreply.github.com>
2024-04-06 09:55:20 +0200

    Testing CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import pickle
 import numpy as np
 from typing import List
@@ -5,18 +6,19 @@ from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
 
-
+# Initialize the FastAPI app with a title
 app = FastAPI(title="Predicting Wine Class with batching")
 
-# Open classifier in global scope
+# Load the pre-trained classifier from a file in the global scope
+# This is done outside of request handling function to load the model only once at the start
 with open("models/wine.pkl", "rb") as file:
     clf = pickle.load(file)
 
-
+# Define a Pydantic model for the request body
 class Wine(BaseModel):
     batches: List[conlist(item_type=float, min_length=13, max_length=13)]
 
-
+# Define a route for predictions using a POST request
 @app.post("/predict")
 def predict(wine: Wine):
     batches = wine.batches
